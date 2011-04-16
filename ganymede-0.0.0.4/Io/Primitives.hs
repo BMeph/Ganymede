ef1419e778073698c7c3f250275386543400c295
Walt Rorie-Baety <black.meph@gmail.com>
2011-04-15 20:23:57 -0700

    made variables look more "Haskelly"

---
+++ ganymede-0.0.0.4/Io/Primitives.hs
@@ -0,0 +1,283 @@
+﻿{------------------------------------------------------------
+(************************************************************ 
+ Io Default Primitive Continuation Functions                  
+   Based on:
+   Amalthea, ml             
+   Martin Sandin (d97masa@dtek.chalmers.se)
+   translated to Haskell by Walt "BMeph" Rorie-Baety
+     (black.meph@gmail.com)  
+ ************************************************************)
+-------------------------------------------------------------}
+module Io.Primitives where
+
+import Io.Util ((.>), int_of_string, print_, subStr)
+import Io.Types
+
+import Control.Exception (throw)
+import Data.List (genericLength)
+import Data.Map as M (Map, fromList) -- for commandMap
+import System.IO.Unsafe (unsafePerformIO)
+
+-- PrimFuncs :: [IoCont] -> (IoCont,[IoCont])
+-- I.e., the internal values of IoCont's CPrim.
+-- The argument is "the operand stack", the portion of
+--  the execution stack looked at for the purpose of
+--  running the function. The result is a pair of the
+-- main result, and the unused part of the stack.
+
+terminate     :: PrimFunc
+addInt        :: PrimFunc
+multInt       :: PrimFunc
+powInt        :: PrimFunc
+divInt        :: PrimFunc
+subInt        :: PrimFunc
+eqCont        :: PrimFunc
+ltInt         :: PrimFunc
+gtInt         :: PrimFunc
+printInt      :: PrimFunc
+printInt_     :: PrimFunc
+stringFromInt :: PrimFunc
+intFromString :: PrimFunc
+concString    :: PrimFunc
+subString     :: PrimFunc
+stringLength  :: PrimFunc
+printString   :: PrimFunc
+printString_  :: PrimFunc
+writeStar     :: PrimFunc
+write         :: PrimFunc
+writeLn       :: PrimFunc
+
+terminate _  = (CTerminate, [])
+
+{-
+-- (* Concurrency operators: par, chan *)
+parExec [cont_alt, c] = unsafePerformIO (forkIO (miniRTS cont_alt) .> (c, []))
+parExec       _       = throw (IoPrimError "par")
+
+mkChan [c] = let x = supplyChanID in (c, [CChanIn x, CChanOut x])
+mkChan _  = throw (IoPrimError "create chan")
+-}
+
+-- (* Integer operations *)
+addInt [CInt na, CInt nb, c] = (c, [CInt (na + nb)])
+addInt       _               = throw (IoPrimError "+")
+
+multInt [CInt na, CInt nb, c] = (c, [CInt (na * nb)])
+multInt             _         = throw (IoPrimError "*") 
+
+powInt [CInt na, CInt nb, c] = (c, [CInt (na ^ nb)])
+powInt            _          = throw (IoPrimError "^") 
+
+divInt [CInt na, CInt nb, c]
+   |  nb == 0  = throw (IoPrimError "/: division by zero")
+   | otherwise = (c, [CInt (div na nb)])
+divInt _ = throw (IoPrimError "/") 
+  
+subInt [CInt na, CInt nb, c] = (c, [CInt (na - nb)])
+subInt _ = throw (IoPrimError "-") 
+  
+eqCont [CInt na, CInt nb, ctrue, cfalse] = 
+    (if na == nb then ctrue else cfalse, [])
+eqCont [CString na, CString nb, ctrue, cfalse] =
+    (if na == nb then ctrue else cfalse, [])
+eqCont [_, _, _, cfalse] = (cfalse,[])
+eqCont _                     = throw (IoPrimError "=")
+
+ltInt [CInt na, CInt nb, ctrue, cfalse] = (if na < nb then ctrue else cfalse, [])
+ltInt _ = throw (IoPrimError "<")
+
+gtInt [CInt na, CInt nb, ctrue, cfalse] = (if na > nb then ctrue else cfalse, [])
+gtInt _ = throw (IoPrimError ">")
+
+printInt [CInt n, c] = unsafePerformIO (print n .> (c, []))
+printInt _ = throw (IoPrimError "print_int")
+
+printInt_ [CInt n, c] = unsafePerformIO (print_ n .> (c, []))
+printInt_      _      = throw (IoPrimError "print_int_")
+
+stringFromInt [CInt n, c] = (c, [CString (show n)])
+stringFromInt      _      = throw (IoPrimError "string_of_int")
+
+intFromString [CString n, c] = case int_of_string n of
+    Just x ->  (c, [CInt x])
+    _      -> throw (IoPrimError "int_of_string: string not valid integer")
+intFromString _ = throw (IoPrimError "int_of_string")
+
+-- (* String operation *)
+concString [CString sa, CString sb, c] =
+    (c, [CString (sa ++ sb)])
+concString _ = throw (IoPrimError "++")
+
+subString [CString sa, CInt is, CInt il, c] = 
+  (c, [CString (subStr is il sa)])
+subString    _    = throw (IoPrimError "substring")
+  
+stringLength [CString sa, c] = (c, [CInt (genericLength sa)])
+stringLength         _       = throw (IoPrimError "string_length")
+  
+printString [CString s, c] = unsafePerformIO
+    (print s .> (c, []))
+printString       _        = throw (IoPrimError "print_string")
+
+printString_ [CString s, c] = unsafePerformIO
+    (print_ s  .> (c, []))
+printString_         _      = throw (IoPrimError "print_string_")
+
+writeStar [c] = write [c,CTerminate]
+writeStar  _  = throw (IoPrimError "write*")
+
+write [CString s, c] = printString_ [CString s, c]
+write [CInt    n, c] = printInt_    [CInt    n, c]
+write       _        = throw (IoPrimError "write")
+
+writeLn [CString s, c] = printString [CString s, c]
+writeLn [CInt    n, c] = printInt    [CInt    n, c]
+writeLn        _       = throw (IoPrimError "writeLn")
+  
+commandMap :: M.Map String PrimFunc
+commandMap = M.fromList  
+  [("terminate", terminate),
+   ("int_add",addInt),
+   ("int_sub",subInt),
+   ("int_mul",multInt),
+   ("int_div",divInt),
+   ("int_exp",powInt),
+-- ("par",parExec),
+-- ("chan",mkChan),
+   ("eq",eqCont),
+   ("int_lt",ltInt),
+   ("int_gt",gtInt),
+   ("int_print_endline",printInt),
+   ("int_print",printInt_),
+   ("string_concat",concString),
+   ("write_endline",writeLn),
+   ("write",write),
+   ("write_and_stop",writeStar),
+   ("string_print",printString_),
+   ("int_to_string",stringFromInt),
+   ("string_sub",subString),
+   ("string_length",stringLength),
+   ("string_to_int",intFromString)]
+
+{-
+open Io_types;;
+
+(* Misc operations *)
+let terminate = function
+  | [] -> (Cterminate, [])
+  | _ -> raise (Io_prim_error "terminate")
+
+(* Integer operations *)
+let addint = function
+  | [Cint na; Cint nb; c] -> (c, [Cint (na + nb)])
+  | _ -> raise (Io_prim_error "+")
+
+let multint = function
+  | [Cint na; Cint nb; c] -> (c, [Cint (na * nb)])
+  | _ -> raise (Io_prim_error "*") 
+
+let divint = function
+  | [Cint na; Cint nb; c] ->
+    (try
+      (c, [Cint (na / nb)])
+    with
+      | Division_by_zero -> raise (Io_prim_error "/: division by zero"))
+  | _ -> raise (Io_prim_error "/") 
+  
+let subint = function
+  | [Cint na; Cint nb; c] -> (c, [Cint (na - nb)])
+  | _ -> raise (Io_prim_error "-") 
+  
+let eqcont = function
+  | [Cint na;Cint nb; ctrue; cfalse] -> ((if na = nb then ctrue else cfalse), [])
+  | [Cstring na; Cstring nb; ctrue; cfalse] -> ((if na = nb then ctrue else cfalse), [])
+  | [ca; cb; ctrue; cfalse] -> (cfalse,[])
+  | _ -> raise (Io_prim_error "=")
+
+let ltint = function
+  | [Cint na; Cint nb; ctrue; cfalse] -> ((if na < nb then ctrue else cfalse), [])
+  | _ -> raise (Io_prim_error "<")
+
+let gtint = function
+  | [Cint na; Cint nb; ctrue; cfalse] -> ((if na > nb then ctrue else cfalse), [])
+  | _ -> raise (Io_prim_error ">")
+
+let printint = function
+  | [Cint n; c] -> print_int n;print_newline (); (c, [])
+  | _ -> raise (Io_prim_error "print_int")
+
+let printint_ = function
+  | [Cint n; c] -> print_int n; (c, [])
+  | _ -> raise (Io_prim_error "print_int_")
+
+let stringfromint = function
+  | [Cint n; c] -> (c, [Cstring (string_of_int n)])
+  | _ -> raise (Io_prim_error "string_of_int")
+
+let intfromstring = function
+  | [Cstring n; c] ->
+    (try
+      (c, [Cint (int_of_string n)])
+    with
+      Failure _ -> raise (Io_prim_error "int_of_string: string not valid integer"))
+  | _ -> raise (Io_prim_error "int_of_string")
+
+(* String operation *)
+let concstring = function
+  | [Cstring sa; Cstring sb; c] -> (c, [Cstring (sa ^ sb)])
+  | _ -> raise (Io_prim_error "^")
+
+let substring = function
+  | [Cstring sa; Cint is; Cint il; c] -> (c, [Cstring (String.sub sa is il)])
+  | _ -> raise (Io_prim_error "substring")
+  
+let string_length = function
+  | [Cstring sa; c] -> (c, [Cint (String.length sa)])
+  | _ -> raise (Io_prim_error "string_length")
+  
+let printstring = function
+  | [Cstring s; c] -> print_endline s; (c, [])
+  | _ -> raise (Io_prim_error "print_string")
+
+let printstring_ = function
+  | [Cstring s; c] -> print_string s; (c, [])
+  | _ -> raise (Io_prim_error "print_string_")
+  
+let map =
+  [("terminate", terminate);
+   ("int_add",addint);
+   ("int_sub",subint);
+   ("int_mul",multint);
+   ("int_div",divint);
+   ("eq",eqcont);
+   ("int_lt",ltint);
+   ("int_gt",gtint);
+   ("int_print_endline",printint);
+   ("int_print",printint_);
+   ("string_concat",concstring);
+   ("string_print_endline",printstring);
+   ("string_print",printstring_);
+   ("int_to_string",stringfromint);
+   ("string_sub",substring);
+   ("string_length",string_length);
+   ("string_to_int",intfromstring);]
+
+ -} 
+
+{-
+Observations:
+OCaml lets you mix effectful statements with expressions.
+ This will "force" this section to return an (IO a) type.
+Perhaps not OCaml, but the author did not care for using
+ the CPS of the interpreted language inside the inter-
+ preter itself. I will reserve judgement on if this was
+ a good design choice. I definitely makes it clear when
+ details of the internals, versus the interpreted items,
+ are being used.
+This seems to be a very OO method (no pun intended) to do
+ interpretation. For instance, all of these "primitive"
+ functions could be in a record, instead of a map.
+
+ -}
+ 
+ 
---
+++ ganymede-0.0.0.4/Io/Util.hs
@@ -0,0 +1,192 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{------------------------------------------------------------
+ module Io.FileUtil utility module
+   Based on:
+   Amalthea, fileutil.ml
+   Martin Sandin (d97masa@dtek.chalmers.se)
+   translated to Haskell in 2011 by Walt "BMeph" Rorie-Baety
+     (black.meph@gmail.com)
+ ************************************************************)
+-------------------------------------------------------------}
+
+module Io.Util where
+
+import Io.Types
+
+import Prelude hiding (catch)
+import Control.Exception (IOException, throwIO)
+import Control.Monad (filterM,liftM2)
+import Data.IORef (modifyIORef, newIORef, readIORef, writeIORef)
+import Data.List (genericDrop, genericTake)
+import qualified Data.Map as M (Map, filter, insert, map)
+import Data.Maybe (listToMaybe) -- for string_to_int
+import System.Console.GetOpt (OptDescr, usageInfo)
+import System.Directory ( doesFileExist, getDirectoryContents
+                        , getPermissions, Permissions(..))
+import System.FilePath ((</>))
+import System.IO.Error (doesNotExistErrorType,mkIOError,annotateIOError)
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Text.Parsec.Pos as P (initialPos, newPos)
+-- import Control.Exception (throw)
+
+infixl 2 ><
+infixl 1 >-
+
+-- (* Util *)
+freshPos :: Pos
+freshPos = P.newPos "" 0 0
+
+initialPos :: String -> Pos
+initialPos = P.initialPos
+
+resetPos :: String -> Pos  -> Pos 
+resetPos mname = \_ -> (initialPos mname)
+
+showsPos :: Pos -> ShowS
+showsPos = shows
+
+-- For-convenience IORef-manipulating functions
+ref :: a -> IORef a
+fetch :: IORef a -> a
+(<--) :: IORef a -> a -> IO ()
+(=:) :: IORef a -> a -> ()
+(<-<) :: IORef a -> (a -> a) -> IO ()
+
+ref = unsafePerformIO . newIORef
+fetch = unsafePerformIO . readIORef
+(<--) = writeIORef
+ioref =: a = unsafePerformIO (ioref <-- a)
+(<-<) = modifyIORef
+
+-- (* Helper functions *)   
+subStr :: Integer -> Integer -> [a] -> [a]
+subStr b e = genericDrop b . genericTake e
+
+print_ :: (Show a) => a -> IO ()
+print_ = (>> (putStr " ")). putStr . show
+
+unsafePerformIOIoCont :: (IoCont -> IO a) -> (IoCont, IoCont) -> (IoCont, [IoCont])
+unsafePerformIOIoCont f (iob, ioa) = unsafePerformIO (f ioa .> (iob, []))
+-- == ((unsafePerformIO .) . (. (,[])) . (.>) . f) ioa $ iob
+
+int_of_string :: (Read i, Num i) => String -> Maybe i
+int_of_string = listToMaybe . map (+0) . fst . unzip . reads
+
+filMap :: (Ord k) => (a -> b) -> (a -> Bool) -> M.Map k a -> M.Map k b
+filMap f p = M.map f . M.filter p
+
+boolToEither :: a -> (a -> Bool) -> Either a a
+boolToEither a p |    p a    = Right a
+                 | otherwise =  Left a
+
+(.>) :: (Functor m) => m b -> a -> m a
+-- "mb .> a" uses mb's effects, and returns a
+(.>) = (.const).flip fmap
+
+(><) :: (Monad m) => m a -> m b -> m (a, b)
+(><) = liftM2 (,)
+
+(/>) :: (Monad m) => m a -> m b -> m a
+ma /> mb = do { a <- ma; _ <- mb; return a}
+
+(>-) :: (Functor f) => f a -> (a -> b) -> f b
+fa >- f = fmap f fa
+
+-- bool: like maybe, foldr or either, it deconstructs a Bool into
+--  some other type of your choice.
+bool :: t -> t -> Bool -> t
+bool false  _   False = false
+bool   _   true True  = true
+
+ifte :: Bool -> t -> t -> t
+ifte p t f = bool f t p
+
+showBracket :: ShowS -> ShowS
+showBracket p = showChar '[' . p . showChar ']'
+
+interC' xs  = uncurry ((. flip foldr [] (((xs ++) .) . (++))) . (++) . concat) . splitAt 1
+-- ^ I.e., interC' xs xss = let ~(xsHead, xsTail) = splitAt 1 xss in
+--  concat xsHead ++
+--    foldr (\xssElem xssRest -> xs ++ xssElem ++ xssRest) [] xsTail
+
+splitString :: Char -> String -> [String]
+splitString divider string =
+  case dropWhile (/= divider) string of
+    "" -> []
+    s' -> w : splitString divider (drop 1 s'')
+          where (w, s'') =
+                 break (/= divider) s'
+
+uncIns :: (Ord a) => (a, b) -> M.Map a b -> M.Map a b
+first  :: (a -> b) -> (a, c) -> (b, c)
+second :: (a -> b) -> (c, a) -> (c, b)
+uncIns = uncurry M.insert
+first  f (x, y) = (f x,   y)
+second g (x, y) = (  x, g y)
+
+{- Misc utility functions relating to errors -}
+
+ganyOptErr :: String -> [OptDescr a] -> String -> IO b
+ganyOptErr useBlurb opts errs = ioError (userError (errs ++ usageInfo useBlurb opts))
+
+ganyRunErr :: IOException -> IO a
+ganyRunErr e = ioError (annotateIOError e "Ganymede: " Nothing Nothing)
+{- Misc utility functions relating to files -}
+                 
+find_file :: FilePath -> FilePath -> IO FilePath
+find_file dir_name file_name = fileSearch file_name [dir_name]
+
+fileSearch :: FilePath -> [FilePath] -> IO FilePath
+fileSearch fName dNames = fSF dNames where
+ fSF [] = throwIO (mkIOError doesNotExistErrorType "find_file" Nothing (Just fName))
+ fSF ds = do
+  foundDirs <- filterM doesFileExist $ (map (</> fName) ds)
+  if null foundDirs
+    then do
+      moreDirs <- mapM getSubDirs ds
+      fSF (concat moreDirs)
+    else return (head foundDirs)
+
+getSubDirs :: FilePath -> IO [FilePath]
+getSubDirs dir = do
+  dirsAndFiles <- getDirectoryContents dir
+  dirs <- filterM isDirectory dirsAndFiles
+  return (filter (\d -> (d /= ".") && (d /= "..")) dirs)
+
+isDirectory :: FilePath -> IO Bool
+isDirectory f = fmap (\pers -> searchable pers && not (executable pers)) (getPermissions f)
+
+{- original Ocaml:
+(* Misc utility functions relating to files *)
+
+let rec find_file : string -> string -> string
+= fun dir_name file_name ->
+  if Sys.file_exists (Filename.concat dir_name file_name) then
+    Filename.concat dir_name file_name
+  else
+    let dir = Unix.opendir dir_name in
+    let path = ref "" in
+      (try
+        while !path = "" do
+          let entry = Unix.readdir dir in
+          match entry with
+            | "." -> ()
+            | ".." -> ()
+            | _ ->
+              let entry = Filename.concat dir_name entry in
+              match (Unix.stat entry).Unix.st_kind with
+                | Unix.S_DIR ->
+                  (try
+                    path := find_file entry file_name
+                  with
+                    | Unix.Unix_error (Unix.ENOENT, _, _) -> ())
+                | _ -> ()
+        done
+      with
+        End_of_file -> ());
+      if (!path) = "" then
+        raise (Unix.Unix_error (Unix.ENOENT, "find_file", file_name))
+      else
+        !path
+
+ -}        
